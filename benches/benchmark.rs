1b02815d8b04fdaea44a0b2c6ad21db4db454ba8
D <d.schulte@uu.nl>
2021-01-25 00:29:32 +0100

    Updated benchmarks to return a csv file

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,110 +1,129 @@
 use pdbtbx::*;
-use std::cmp::min;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufWriter;
 use std::time::{Duration, Instant};
 
 fn main() {
-    let (small, _) = open("example-pdbs/1ubq.pdb", StrictnessLevel::Loose).unwrap();
-    let (big, _) = open("example-pdbs/pTLS-6484.pdb", StrictnessLevel::Loose).unwrap();
+    // Setup the data needed
+    let names = vec![
+        ("small", "example-pdbs/1ubq.pdb"),
+        ("medium", "example-pdbs/1yyf.pdb"),
+        ("big", "example-pdbs/pTLS-6484.pdb"),
+    ];
+    let mut models = Vec::with_capacity(names.len());
+    for (name, path) in &names {
+        models.push((*name, open(path, StrictnessLevel::Loose).unwrap().0))
+    }
+    let mut results = Vec::new();
 
-    measure(bench_open, &"example-pdbs/1ubq.pdb", "Open - small", None);
-    measure(
-        bench_open,
-        &"example-pdbs/pTLS-6484.pdb",
-        "Open - big",
-        None,
-    );
-    measure(bench_transformation, &small, "Transformation - small", None);
-    measure(bench_transformation, &big, "Transformation - big", None);
-    measure(bench_remove, &small, "Remove - small", None);
-    measure(bench_remove, &big, "Remove - big", None);
-    measure(bench_iteration, &small, "Iteration - small", None);
-    measure(bench_iteration, &big, "Iteration - big", None);
-    measure(bench_validate, &small, "Validate - small", Some(1000));
-    measure(bench_validate, &big, "Validate - big", Some(1000));
-    measure(bench_renumber, &small, "Renumber - small", None);
-    measure(bench_renumber, &big, "Renumber - big", None);
-    measure(bench_clone, &small, "Clone - small", None);
-    measure(bench_clone, &big, "Clone - big", None);
-    measure(bench_save, &small, "Save - small", None);
-    measure(bench_save, &big, "Save - big", None);
+    // Do the benchmarking
+    results.extend(measure_multiple(bench_open, &names, "Open"));
+    results.extend(measure_multiple(
+        bench_transformation,
+        &models,
+        "Transformation",
+    ));
+    results.extend(measure_multiple(bench_remove, &models, "Remove"));
+    results.extend(measure_multiple(bench_iteration, &models, "Iteration"));
+    results.extend(measure_multiple(bench_validate, &models, "Validate"));
+    results.extend(measure_multiple(bench_renumber, &models, "Renumber"));
+    results.extend(measure_multiple(bench_clone, &models, "Clone"));
+    results.extend(measure_multiple(bench_save, &models, "Save"));
+
+    // Save the results to a csv
+    let file = File::create("dump/benchmark_results.csv").unwrap();
+    let mut sink = BufWriter::new(file);
+    sink.write_all("Name,Average(ns),StandardDeviation(ns),Runs\n".as_bytes())
+        .unwrap();
+    for item in results {
+        sink.write_fmt(format_args!(
+            "{},{},{},{}\n",
+            item.0, item.1, item.2, item.3
+        ))
+        .unwrap();
+    }
+    sink.flush().unwrap();
 }
 
-fn bench_open(filename: &str) -> Duration {
-    let now = Instant::now();
+fn bench_open(filename: &str) {
     let (_pdb, _errors) = open(filename, StrictnessLevel::Loose).unwrap();
-    now.elapsed()
 }
 
-fn bench_transformation(mut pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_transformation(mut pdb: PDB) {
     let transformation = TransformationMatrix::rotation_x(90.0);
     pdb.apply_transformation(&transformation);
-    now.elapsed()
 }
 
-fn bench_remove(mut pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_remove(mut pdb: PDB) {
     pdb.remove_atoms_by(|atom| atom.serial_number() % 2 == 0);
-    now.elapsed()
 }
 
-fn bench_iteration(pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_iteration(pdb: PDB) {
     let mut _average = 0.0;
     for atom in pdb.atoms() {
         _average += atom.b_factor();
     }
     _average /= pdb.atom_count() as f64;
-    now.elapsed()
 }
 
-fn bench_validate(pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_validate(pdb: PDB) {
     validate(&pdb);
-    now.elapsed()
 }
 
-fn bench_renumber(mut pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_renumber(mut pdb: PDB) {
     pdb.renumber();
-    now.elapsed()
 }
 
-fn bench_clone(pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_clone(pdb: PDB) {
     let _copy = pdb.clone();
-    now.elapsed()
 }
 
-fn bench_save(pdb: PDB) -> Duration {
-    let now = Instant::now();
+fn bench_save(pdb: PDB) {
     save(pdb, "dump/dump.pdb", StrictnessLevel::Loose).unwrap();
-    now.elapsed()
+}
+
+fn measure_multiple<T: Clone>(
+    function: fn(T),
+    subjects: &[(&str, T)],
+    description: &str,
+) -> Vec<(String, u128, u128, usize)> {
+    let mut output = Vec::with_capacity(subjects.len());
+    for (name, item) in subjects {
+        output.push(measure(
+            function,
+            &item,
+            &format!("{} - {}", description, name),
+        ));
+    }
+    output
 }
 
 fn measure<T: Clone>(
-    function: fn(T) -> Duration,
+    function: fn(T),
     subject: &T,
     description: &str,
-    max: Option<u128>,
-) {
+) -> (String, u128, u128, usize) {
     let mut times = Vec::new();
     let _ = function(subject.clone());
-    let now = Instant::now();
+    let start = Instant::now();
+    let mut now;
 
     for _ in 0..5 {
-        times.push(function(subject.clone()));
+        let clone = subject.clone();
+        now = Instant::now();
+        function(clone);
+        times.push(now.elapsed());
     }
 
-    let average = now.elapsed().checked_div(5).unwrap();
+    let average = start.elapsed().checked_div(5).unwrap();
 
     // Lets run for 3 more seconds, including cloning of the subject
-    let mut runs = 3_000_000_000 / average.as_nanos();
-    if let Some(n) = max {
-        runs = min(n, runs);
-    }
-    for _ in 0..runs {
-        times.push(function(subject.clone()));
+    for _ in 0..3_000_000_000 / average.as_nanos() {
+        let clone = subject.clone();
+        now = Instant::now();
+        function(clone);
+        times.push(now.elapsed());
     }
 
     let average = times
@@ -122,38 +141,11 @@ fn measure<T: Clone>(
     }
     deviation /= times.len() as f64;
     deviation = deviation.sqrt();
-    let standard_deviation = Duration::from_secs_f64(deviation / 1_000_000_000.0);
 
-    println!(
-        "{}: average time over {} runs:\n    {} ± {} ={:6.2}%\n",
-        description,
+    (
+        description.to_string(),
+        average.as_nanos(),
+        deviation as u128,
         times.len(),
-        pretty_print(average),
-        pretty_print(standard_deviation),
-        deviation / (average.as_nanos() as f64) * 100.0
-    );
-}
-
-fn pretty_print(duration: Duration) -> String {
-    if duration.as_secs() > 0 {
-        format!(
-            "{:3}s {:3}ms",
-            duration.as_secs(),
-            duration.as_millis() - (duration.as_secs() * 1000) as u128
-        )
-    } else if duration.as_millis() > 0 {
-        format!(
-            "{:3}ms {:3}μs",
-            duration.as_millis(),
-            duration.as_micros() - (duration.as_millis() * 1000) as u128
-        )
-    } else if duration.as_micros() > 0 {
-        format!(
-            "{:3}μs {:3}ns",
-            duration.as_micros(),
-            duration.as_nanos() - (duration.as_micros() * 1000) as u128
-        )
-    } else {
-        format!("     {:4}ns", duration.as_nanos())
-    }
+    )
 }
