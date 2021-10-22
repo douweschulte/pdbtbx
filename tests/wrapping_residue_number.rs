8fc5d35e06e796cb865f695d9e01d308037ce504
D <d.schulte@uu.nl>
2021-10-22 13:56:28 +0200

    Updated test for #66

--- tests/wrapping_residue_number.rs
+++ tests/wrapping_residue_number.rs
@@ -1,7 +1,6 @@
 use pdbtbx::*;
-use std::env;
-use std::path::Path;
 
+/// Open a test file containing 87449 waters so with more than 29000 residues which leads to residue serial numbers that are wrapped
 #[test]
 fn wrapping_residue_number() {
     let (pdb, errors) = pdbtbx::open("example-pdbs/eq.pdb", StrictnessLevel::Strict).unwrap();
@@ -9,5 +8,24 @@ fn wrapping_residue_number() {
     let (pdb2, _) = pdbtbx::open("dump/eq.pdb", StrictnessLevel::Strict).unwrap();
     print!("{:?}", errors);
     print!("{:?}", pdb_errors);
+    // See that the original file is the same as saved and reopened
     assert_eq!(pdb, pdb2);
+    // See that it is possible to select atom with 'impossible' residue serial numbers according to the PDB definition
+    // These are made by adding 10000 to the residue serial number every time a wrap is detected (9999 followed by 0)
+    assert_eq!(
+        pdb.residues()
+            .find(|r| r.serial_number() == 10005)
+            .unwrap()
+            .name()
+            .unwrap(),
+        "HOH"
+    );
+    assert_eq!(
+        pdb.residues()
+            .find(|r| r.serial_number() == 20250)
+            .unwrap()
+            .name()
+            .unwrap(),
+        "HOH"
+    );
 }
