c3dedf0a41f9d1d42492abd6d275c58870bd4a1d
Douwe Schulte <d.schulte@uu.nl>
2022-08-02 02:29:11 +0200

    Added links to useful wwpdb pages to docs

--- src/general_docs/mod.rs
+++ src/general_docs/mod.rs
@@ -1,6 +1,11 @@
 //! Here you can find high level documentation for this crate.
 //! * About saving: [`mod@save`]
 //! * About the PDB hierarchy used: [`structs`]
+//!
+//! Good references for more in depth information:
+//! * PDB: [spec](https://www.wwpdb.org/documentation/file-format-content/format33/v3.3.html)
+//! * mmCIF: [spec](https://mmcif.wwpdb.org/dictionaries/mmcif_pdbx_v50.dic/Index/) [docs](https://mmcif.wwpdb.org/dictionaries/mmcif_pdbx_v50.dic/Groups/index.html)
+//! * PDB to mmCIF conversion: [wwpdb](https://mmcif.wwpdb.org/docs/pdb_to_pdbx_correspondences.html)
 
 use crate::*;
 
