5da9eda972b2ec4a1b58532dc978db12f996e50b
Oliver Wissett <wissettol@gmail.com>
2023-08-25 21:36:40 +0100

    name change linked to PR #110

--- src/structs/helper.rs
+++ src/structs/helper.rs
@@ -19,14 +19,14 @@ pub fn valid_identifier(text: impl AsRef<str>) -> bool {
 
 /// Creates a valid identifier from the given string slice.
 /// Also turns the identifier to uppercase.
-pub fn prepare_identifier(text: impl AsRef<str>) -> Option<String> {
+pub fn prepare_identifier_uppercase(text: impl AsRef<str>) -> Option<String> {
     let text = text.as_ref();
-    prepare_identifier_case_sensitive(text).map(|s| s.to_uppercase())
+    prepare_identifier(text).map(|s| s.to_uppercase())
 }
 
 /// Creates a valid identifier from the given string slice.
 /// Does not change the case.
-pub fn prepare_identifier_case_sensitive(text: impl AsRef<str>) -> Option<String> {
+pub fn prepare_identifier(text: impl AsRef<str>) -> Option<String> {
     let text = text.as_ref();
     (valid_identifier(text) && !text.trim().is_empty()).then(|| text.trim().to_string())
 }
