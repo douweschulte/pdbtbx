7d3d3095e03e1f2301d78042413c0a0e23cab82b
Oliver Wissett <ow257@cam.ac.uk>
2024-04-15 15:06:21 +0100

    fmt

--- src/read/mod.rs
+++ src/read/mod.rs
@@ -10,6 +10,6 @@ mod pdb;
 use super::check_extension;
 
 pub use general::{open, open_gz, open_raw};
-pub use mmcif::{open_mmcif, open_mmcif_raw, open_mmcif_bufread};
+pub use mmcif::{open_mmcif, open_mmcif_bufread, open_mmcif_raw};
 pub use pdb::{open_pdb, open_pdb_raw};
 pub use read_options::{Format, ReadOptions};
