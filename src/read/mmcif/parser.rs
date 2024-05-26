98de870688589ac43cbf843b28914478716e4724
y1zhou <zhou.zy.yi@gmail.com>
2024-05-26 22:10:16 +0800

    feat(read-opts): remove level argument from read_mmcif

--- src/read/mmcif/parser.rs
+++ src/read/mmcif/parser.rs
@@ -14,11 +14,8 @@ use std::io::prelude::*;
 /// # Related
 /// If you want to open a file from memory see [`open_mmcif_raw`]. There is also a function to open a PDB file directly
 /// see [`crate::open_pdb`]. If you want to open a general file with no knowledge about the file type see [`crate::open`].
-pub fn open_mmcif(
-    filename: impl AsRef<str>,
-    level: StrictnessLevel,
-) -> Result<(PDB, Vec<PDBError>), Vec<PDBError>> {
-    open_mmcif_with_options(filename, ReadOptions::default().set_level(level))
+pub fn open_mmcif(filename: impl AsRef<str>) -> Result<(PDB, Vec<PDBError>), Vec<PDBError>> {
+    open_mmcif_with_options(filename, &ReadOptions::default())
 }
 
 /// Parse the given mmCIF file into a PDB struct with [`ReadOptions`].
